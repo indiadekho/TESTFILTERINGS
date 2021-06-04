1a8058c7885c29ed2e2f5af92e8bc1916b76c293
indiadekho <73731918+indiadekho@users.noreply.github.com>
2021-06-04 22:44:34 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -243,7 +243,7 @@ async def help(client, message):
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("NEED HELP?", url="https://telegram.dog/Insane_Banda),
+                    InlineKeyboardButton("NEED HELP?", url="https://telegram.dog/Insane_Banda"),
                     InlineKeyboardButton("About Me", callback_data="about_data")
                 ],
                 [
